c4f4284cd80462711119845057aaf6fb2e792121
WellingtonFidelis <wellingtonf20@gmail.com>
2021-10-23 20:42:54 -0300

    Development calculator.

--- python-fundamentals-data-analysis-3.0/PythonFundamentos/Cap03/Lab02/calculadora_v1.py
+++ python-fundamentals-data-analysis-3.0/PythonFundamentos/Cap03/Lab02/calculadora_v1.py
@@ -1,9 +1,45 @@
+"""
+Created at 10/2021
+By Wellington Fidelis
+"""
 # Calculadora em Python
 
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos 2 e 3. 
-# A solução será apresentada no próximo capítulo!
-# Assista o vídeo com a execução do programa!
 
-print("\n******************* Python Calculator *******************")
+def calculateResult(operation):
+  firstNumber = float(input('Digite o 1º número: '))
+  secondNumber = float(input('Digite o 2º número: '))
+  if (operation == '+'):
+      result = firstNumber + secondNumber
+  elif (operation == '-'):
+      result = firstNumber - secondNumber
+  elif (operation == '/'):
+      result = firstNumber / secondNumber
+  elif (operation == '*'):
+      result = firstNumber * secondNumber
+
+  print('\n{} + {} = {}'.format(firstNumber, secondNumber, result))
 
 
+userOption = -1
+
+while (int(userOption) != 0):
+    print("\n*** Python Calculator ***\n")
+    print('---------- Menu ----------')
+    print('Somar ---------------- [1]')
+    print('Subtrair ------------- [2]')
+    print('Dividir -------------- [3]')
+    print('Multiplicação -------- [4]')
+    print('Sair ----------------- [0]')
+
+    userOption = input('\nEscolha uma opção: ')
+
+    if (int(userOption) > 4):
+      print('\nInsira um valor permitido.')
+    elif (int(userOption) == 1):
+      calculateResult('+')
+    elif (int(userOption) == 2):
+      calculateResult('-')
+    elif (int(userOption) == 3):
+      calculateResult('/')
+    elif (int(userOption) == 4):
+      calculateResult('*')
