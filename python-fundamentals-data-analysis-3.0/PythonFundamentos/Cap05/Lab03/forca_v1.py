a7f5844eb6f0ad47d8345101c2a7f76080972efe
WellingtonFidelis <wellingtonf20@gmail.com>
2021-11-27 16:44:14 -0300

    feat(exercise): finishing Hangman game

--- python-fundamentals-data-analysis-3.0/PythonFundamentos/Cap05/Lab03/forca_v1.py
+++ python-fundamentals-data-analysis-3.0/PythonFundamentos/Cap05/Lab03/forca_v1.py
@@ -1,117 +1,156 @@
 # -*- coding: utf-8 -*-
-# Hangman Game (Jogo da Forca) 
+# Hangman Game (Jogo da Forca)
 # Programação Orientada a Objetos
 
 # Import
 import random
+from os import system
 
-# Board (tabuleiro)
+# Board
 board = ['''
 
 >>>>>>>>>>Hangman<<<<<<<<<<
+                          |
+    +---+                 |
+    |   |                 |
+    |                     |
+    |                     |
+    |                     |
+    |                     |
+===========================''', '''
 
-    +---+
-    |   |
-    |
-    |
-    |
-    |
-=========''', '''
+>>>>>>>>>>Hangman<<<<<<<<<<
+                          |
+    +---+                 |
+    |   |                 |
+    |   O                 |
+    |                     |
+    |                     |
+    |                     |
+===========================''', '''
 
-    +---+
-    |   |
-    |   O
-    |
-    |
-    |
-=========''', '''
+>>>>>>>>>>Hangman<<<<<<<<<<
+                          |
+    +---+                 |
+    |   |                 |
+    |   O                 |
+    |   |                 |
+    |                     |
+    |                     |
+===========================''', '''
 
-    +---+
-    |   |
-    |   O
-    |   |
-    |
-    |
-=========''', '''
+>>>>>>>>>>Hangman<<<<<<<<<<
+                          |
+    +---+                 |
+    |   |                 |
+    |   O                 |
+    |  /|                 |
+    |                     |
+    |                     |
+===========================''', '''
 
-    +---+
-    |   |
-    |   O
-    |  /|
-    |
-    |
-=========''', '''
+>>>>>>>>>>Hangman<<<<<<<<<<
+                          |
+    +---+                 |
+    |   |                 |
+    |   O                 |
+    |  /|\                |
+    |                     |
+    |                     |
+===========================''', '''
 
-    +---+
-    |   |
-    |   O
-    |  /|\
-    |
-    |
-=========''', '''
+>>>>>>>>>>Hangman<<<<<<<<<<
+                          |
+    +---+                 |
+    |   |                 |
+    |   O                 |
+    |  /|\                |
+    |  /                  |
+    |                     |
+===========================''', '''
 
-    +---+
-    |   |
-    |   O
-    |  /|\
-    |  /
-    |
-=========''', '''
-
-    +---+
-    |   |
-    |   O
-    |  /|\
-    |  / \
-    |
-=========''']
+>>>>>>>>>>Hangman<<<<<<<<<<
+                          |
+    +---+                 |
+    |   |                 |
+    |   O                 |
+    |  /|\                |
+    |  / \                |
+    |                     |
+===========================''']
 
 
 # Classe
 class Hangman:
     # Método Construtor
     def __init__(self, word):
-        pass
+        self.word = word
+        self.missed_letters = []
+        self.guessed_letters = []
 
     # Método para adivinhar a letra
     def guess(self, letter):
-        pass
+        if letter in self.word and letter not in self.guessed_letters:
+            self.guessed_letters.append(letter)
+        elif letter not in self.word and letter not in self.missed_letters:
+            self.missed_letters.append(letter)
+        else:
+            return False
+        return True
 
     # Método para verificar se o jogo terminou
     def hangman_over(self):
-        pass
+        return self.hangman_won() or (len(self.missed_letters) == 6)
 
     # Método para verificar se o jogador venceu
     def hangman_won(self):
-        pass
+        if '_' not in self.hide_word():
+            return True
+        return False
 
     # Método para não mostrar a letra no board
     def hide_word(self):
-        pass
+        result = ''
+        for letter in self.word:
+            if letter not in self.guessed_letters:
+                result += '_'
+            else:
+                result += letter
+        return result
 
     # Método para checar o status do game e imprimir o board na tela
     def print_game_status(self):
-        pass
+        print(board[len(self.missed_letters)])
+        print('\nPalavra: ' + self.hide_word())
+        print('\nLetras erradas: ', [letter for letter in self.missed_letters])
+        print()
+        print('\nLetras corretas: ', [letter for letter in self.guessed_letters])
+        print()
 
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
-    path = '/home/tom/Documents/Projects/python-projects/python-fundamentals-data-analysis-3.0/PythonFundamentos/Cap05/Lab03'
-    with open(path + "/palavras.txt", "rt") as f:
+    file_words = './Lab03/palavras.txt'
+    with open(file=file_words, mode='rt', encoding='utf-8') as f:
         bank = f.readlines()
-        print(bank)
-    return bank[random.randint(0, len(bank))].strip()
+    return bank[random.randint(0, len(bank) - 1)].strip()
 
 
 # Função Main - Execução do Programa
 def main():
     # Objeto
     game = Hangman(rand_word())
-    print(game)
+    print(game.word)
     # Enquanto o jogo não tiver terminado, print do status, solicita uma letra
     # e faz a leitura do caracter
+    while not game.hangman_over():
+        system('clear')
+        game.print_game_status()
+        user_input = input('\nDigite uma letra: ')
+        game.guess(user_input)
 
     # Verifica o status do jogo
+    system('clear')
     game.print_game_status()
 
     # De acordo com o status, imprime mensagem na tela para o usuário
@@ -119,7 +158,7 @@ def main():
         print('\nParabéns! Você venceu!!')
     else:
         print('\nGame over! Você perdeu.')
-        print('A palavra era ' + game.word)
+        # print('A palavra era ' + game.word)
 
     print('\nFoi bom jogar com você! Agora vá estudar!\n')
 
