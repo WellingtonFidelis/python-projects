ccd6691b85df226f2630d10e57f37e2f13316f51
WellingtonFidelis <wellingtonf20@gmail.com>
2022-06-22 20:29:08 -0300

    :feat: adding tests files

--- fusion-app/fusion_project/settings.py
+++ fusion-app/fusion_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -148,3 +150,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_PASSWORD = 'password of host'
 # DEFAULT_FROM_EMAIL = 'contato@teste.com.br'
+
+LOGOUT_REDIRECT_URL = 'index'
